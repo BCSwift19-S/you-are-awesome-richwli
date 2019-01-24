7fa395dc1ff667c640c06bae11123a71845b6e1b
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-01-24 14:35:41 -0500

    Section 2.10

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         let msg3 = "I like puppers"
         let msg4 = "I like cattos"
         let msg5 = "I like squirrels"
-        let msg6 = "I like squirtles"
+        let msg6 = "I like Squirtles"
 
         var labelArray: [String] = [msg1,msg2,msg3,msg4,msg5,msg6]
         

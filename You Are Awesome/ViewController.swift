6e1062e0d17ab39d277b214a9cd7697ff7a81b8c
BC Swift Student Loan 1 <bcswiftstudentloan1@csomswiftloan1.local>
2019-01-28 14:39:46 -0500

    Section 2.15

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-    var index = 0
+    @IBOutlet weak var bottomImage: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    var tIndex = 0
+    var pIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("The view loaded huzzah")
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func midButton(_ sender: UIButton) {
+        var textIndex: Int
+        var picIndex: Int
+        var msgArray:[String] = ["Bulbasaur is a swell guy",
+                                 "I dig Bulbasaur!!!","Bulba Bulba",
+                                 "Boba-Saur is my favorite drink!!!",
+                                 "Bulbasaur is a cabbage baby."]
+        
+        repeat{
+            textIndex = Int.random(in: 0..<msgArray.count)
+        }while textIndex == tIndex
+        
+        repeat{
+            picIndex = Int.random(in: 0..<5)
+        }while picIndex == pIndex
+        
+        pIndex = picIndex
+        tIndex = textIndex
+        
+        bottomImage.image = UIImage(named: "bulba\(pIndex)")
+        topLabel.text = msgArray[textIndex]
     
 
 }
 
-
+}
 
